995f61fd2bbc736933c6f185f60f8647709d22df
fedepare <fde014@gmail.com>
2017-05-05 17:33:04 +0200

    cleaning script

--- clean.py
+++ clean.py
@@ -1,5 +1,26 @@
+#!/usr/bin/env python
+
+""" 
+clean.py is a Python script that deletes the files that have been 
+generated using the rpg_davis_simulator [1] and the run.py script after it.
+
+[1] rpg_davis_simulator: https://github.com/uzh-rpg/rpg_davis_simulator
+
+Author: F. Paredes Valles
+Created: 04-05-2017
+"""
+
+import os
 import shutil
 
-shutil.rmtree('aedat')
-shutil.rmtree('src/rpg_davis_simulator/datasets/full_datasets')
-shutil.rmtree('src/rpg_davis_simulator/datasets/rosbags')
+path = 'aedat'
+if os.path.exists(path):
+	shutil.rmtree(path)
+
+path = 'src/rpg_davis_simulator/datasets/full_datasets'
+if os.path.exists(path):
+	shutil.rmtree(path)
+
+path = 'src/rpg_davis_simulator/datasets/rosbags'
+if os.path.exists(path):
+	shutil.rmtree(path)
